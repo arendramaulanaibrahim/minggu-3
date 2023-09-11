314d887f388a7b167921fdb71cf8f9fb1a04d657
arendramaulanaibrahim <118084400+arendramaulanaibrahim@users.noreply.github.com>
2023-09-11 15:31:14 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ VIRTUALENV=.data/venv
 if [ ! -d $VIRTUALENV ]; then python3 -m venv SVIRTUALENV
 fi
 
-if [ ! -f $VIRTUALENV/bin/pip ]; then curl --silent--show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | SVIRTUALE
+if [ ! -f $VIRTUALENV/bin/pip ]; then curl --silent--show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALE
 fi
 
-SVIRTUALENV/bin/pip install -r requirements.txt
+$VIRTUALENV/bin/pip install -r requirements.txt
 
-SVIRTUALENV/bin/python3 app.py
+$VIRTUALENV/bin/python3 app.py
 Footer
